706239e25a006fef1999a2968fa66a8d9cf6aa5b
pashkoff <pashkoff2@gmail.com>
2012-03-11 22:57:41 +0200

    Data collector and saver

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ Created on 04.03.2012
 
 @author: Pashkoff
 '''
+import sys
+
 from DataDecoder import DataDecoder
 from UdpServer import UdpServer
 from PlotLine import PlotLine
@@ -12,81 +14,34 @@ from Event import Event
 import matplotlib.pyplot as plt
 import numpy as np
 
-
-class Filter():
-    '''
-    classdocs
-    '''
+class DataSaver:
     def __init__(self):
-        '''
-        Constructor
-        '''
-        
-        self.on_data = Event()
-        self.acc = np.array([0,0,0])
-        pass
+        self.d = list()
     
     def __call__(self, data):
         self.__on_data(data)
         pass
-        
+    
     def __on_data(self, pba):
-        if self.on_data.have_any():
-            k = 0.2
-            
-            a = np.array([pba.x, pba.y, pba.z])
-            
-            #self.acc = a * k + self.acc * (1 - k)
-            self.acc = self.acc + k*(a - self.acc)
-            r = self.acc
-            
-            class D:
-                pass
-            d = D()
-            d.x = r[0]
-            d.y = r[1]
-            d.z = r[2]
-            
-            self.on_data(d)
-        pass
-    
-    
+        a = np.array([pba.x, pba.y, pba.z])
+        self.d.append(a)
+        
+        print len(self.d) 
+        
+        if len(self.d) >= 1000:
+            np.savetxt('data1000', self.d)
+            sys.exit()
+
 
 def main():
     
     dd = DataDecoder()
-    fil = Filter()
     serv = UdpServer()
     
-    fig = plt.figure()
-    
-    dd.on_data.add(fil)
-    
-    rows = 3
-    cols = 1
-    i = 1
-    for f in ('x', 'y', 'z'):
-        ax = Axis(fig, [rows, cols, i])
-        dd.on_data.add(ax)
-        
-        line = PlotLine(f,color='b')
-        dd.on_data.add(line)
-        ax.add_line(line)
-        
-        linef = PlotLine(f, color='r')
-        fil.on_data.add(linef)
-        ax.add_line(linef)
-        
-        i = i + 1
-        pass
+    saver = DataSaver()
+    dd.on_data.add(saver)
     
     serv.on_read.add(dd)
-    
-    
-    
-    plt.ion()
-    plt.plot()
-    plt.draw()
     
     serv.run()
     
