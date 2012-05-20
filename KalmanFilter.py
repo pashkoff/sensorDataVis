458938489f39ba9a6f9f7d7937079b09ce96547d
pashkoff <pashkoff2@gmail.com>
2012-05-20 19:32:04 +0300

    Another attempt

--- KalmanFilter.py
+++ KalmanFilter.py
@@ -4,32 +4,47 @@ Created on 23.04.2012
 @author: Pashkoff
 '''
 
+import sys
+
 from Event import Event
 
 import numpy as np
+from numpy import dot, sum, tile, linalg, log, exp, pi, array, eye, zeros, diag
+from numpy.linalg import inv, det
+ 
+def kf_predict(X, P, A, Q, B, U):
+    X = dot(A, X) + dot(B, U)
+    P = dot(A, dot(A, A.T)) + Q
+    return (X, P)
 
-class Channel():
-    def __init__(self):
-        self.Q = 1e-5
-        self.xhat = 0
-        self.P = 1
-        self.xhatminus = 0
-        self.Pminus = 0
-        self.K = 0
-        self.R = 0.1 ** 4
-        pass
-    
-    
-    def step(self, z):
-        self.xhatminus = self.xhat
-        
-        self.Pminus = self.P + self.Q
-        
-        self.K = self.Pminus / (self.Pminus + self.R)
-        self.xhat = self.xhatminus + self.K * (z - self.xhatminus)
-        self.P = (1 - self.K) * self.Pminus
-        
-        return self.xhat
+def gauss_pdf(X, M, S): 
+
+    if M.shape[1] == 1: 
+        DX = X - np.tile(M, X.shape[1])   
+        E = 0.5 * sum(DX * (dot(inv(S), DX)), axis=0) 
+        E = E + 0.5 * M.shape[0] * log(2 * pi) + 0.5 * log(det(S)) 
+        P = exp(-E) 
+    elif X.shape[1] == 1: 
+        DX = tile(X, M.shape[1])- M   
+        E = 0.5 * sum(DX * (dot(inv(S), DX)), axis=0) 
+        E = E + 0.5 * M.shape[0] * log(2 * pi) + 0.5 * log(det(S)) 
+        P = exp(-E) 
+    else: 
+        DX = X-M   
+        E = 0.5 * dot(DX.T, dot(inv(S), DX)) 
+        E = E + 0.5 * M.shape[0] * log(2 * pi) + 0.5 * log(det(S)) 
+        P = exp(-E) 
+ 
+    return (P[0],E[0]) 
+
+def kf_update(X, P, Y, H, R):
+    IM = dot(H, X) 
+    IS = R + dot(H, dot(P, H.T)) 
+    K = dot(P, dot(H.T, inv(IS))) 
+    X = X + dot(K, (Y-IM)) 
+    P = P - dot(K, dot(IS, K.T)) 
+    LH = gauss_pdf(Y, IM, IS) 
+    return (X,P,K,IM,IS,LH) 
 
 class KalmanFilter():
     '''
@@ -42,7 +57,27 @@ class KalmanFilter():
         '''
         
         self.on_data = Event()
-        self.ch = [Channel(), Channel(), Channel()]
+        
+        self.X = np.array([[0.0], [0.0], [0.0]]) 
+        self.P = eye(self.X.shape[0]) * 0.1 ** 4  
+        self.A = np.array([[1., 0.,  0.], [0., 1., 0.], [0., 0., 1.]]) 
+        self.Q = eye(self.X.shape[0]) * 0.0001 
+        self.B = eye(self.X.shape[0]) 
+        self.U = zeros((self.X.shape[0], 1))
+        self.H = eye(self.X.shape[0]) 
+        self.R = eye(self.X.shape[0]) * 5 ** 1 
+        
+        print self.X
+        print self.P
+        print self.A
+        print self.Q
+        print self.B
+        print self.U
+        print self.H
+        print self.R
+        
+#        sys.exit()
+        
         
         pass
     
@@ -53,20 +88,19 @@ class KalmanFilter():
     def __on_data(self, pba):
         if self.on_data.have_any():
             
-            a = np.array([pba.x, pba.y, pba.z])
+            Y = np.array([[pba.x], [pba.y], [pba.z]])
             
-            r = list()
-            for i in range(3):
-                z = self.ch[i].step(a[i])
-                r.append(z)
-                pass
+            (self.X, self.P) = kf_predict(self.X, self.P, self.A, self.Q, self.B, self.U)
+            (self.X, self.P, K, IM, IS, LH) = kf_update(self.X, self.P, Y, self.H, self.R)
             
+            print (K, IM, IS, LH)
+             
             class D:
                 pass
             d = D()
-            d.x = r[0]
-            d.y = r[1]
-            d.z = r[2]
+            d.x = self.X[0]
+            d.y = self.X[1]
+            d.z = self.X[2]
             
             self.on_data(d)
         pass
